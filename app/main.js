6782d061a65500ada7cf22a698f786bcb19051a7
James Halliday <mail@substack.net>
2015-07-07 04:38:55 -0700

    preliminary approval api

--- app/main.js
+++ app/main.js
@@ -9,9 +9,9 @@ var main = require('main-loop');
 var vdom = require('virtual-dom');
 
 var EventEmitter = require('events').EventEmitter;
-var bus = new EventEmitter;
+var ui = new EventEmitter;
 
-bus.on('create-wallet', function () {
+ui.on('create-wallet', function () {
     box.createWallet(function (err, wallet) {
         if (err) return showError(err);
         state.wallets.push(wallet);
@@ -19,11 +19,17 @@ bus.on('create-wallet', function () {
     });
 });
 
+box.on('request', function (origin, req) {
+    state.requests.push(req);
+    loop.update(state);
+});
+
 var state = {
     url: location.pathname,
     page: null,
-    bus: bus,
-    wallets: []
+    bus: ui,
+    wallets: [],
+    requests: []
 };
 var loop = main(state, require('./render.js'), vdom);
 document.querySelector('#content').appendChild(loop.target);
@@ -37,15 +43,26 @@ var showPage = singlePage(function (href) {
 var catcher = require('catch-links');
 catcher(window, showPage);
 
-var RPC = require('frame-rpc');
-var origin = document.referrer;
-var rpc = RPC(window, window.parent, origin, {
-    // ... todo ...
+var rpc = require('hello-frame-rpc');
+rpc.listen('*', {
+    request: function (cb) {
+        // ...
+        console.log('GOT REQUEST!');
+        cb(true);
+    }
+}, function (err, remote) {
+    console.log('remote client connected');
 });
 
 box.listWallets(function (err, wallets) {
     if (err) return showError(err);
     state.wallets = wallets;
+    loop.update(state);
+});
+
+box.listRequests(function (err, requests) {
+    if (err) return showError(err);
+    state.requests = requests;
     loop.update(state);
 });
 
--- index.js
+++ index.js
@@ -1,23 +1,135 @@
 var bitcoin = require('bitcoinjs-lib');
-var defined = require('defined');
-var inherits = require('inherits');
-var EventEmitter = require('events').EventEmitter;
+
 var through = require('through2');
 var readonly = require('read-only-stream');
 var pump = require('pump');
+
+var defined = require('defined');
+var inherits = require('inherits');
+var PageBus = require('page-bus');
 var once = require('once');
+var xtend = require('xtend');
+var noop = require('noop');
+
+var defaults = require('levelup-defaults');
+
+var normOrigin = require('./lib/norm_origin.js');
 
 var randomBytes = require('randombytes');
 function defaultRng () { return randomBytes(32) }
 
 module.exports = Box;
+inherits(Box, PageBus);
 
 function Box (db, opts) {
     if (!(this instanceof Box)) return new Box(db, opts);
+    PageBus.call(this);
     if (!opts) opts = {};
     this.rng = defined(opts.rng, defaultRng);
-    this.db = db;
+    this.db = defaults(db, { valueEncoding: 'json' });
 }
+
+Box.prototype.request = function (origin, req, cb) {
+    var self = this;
+    origin = normOrigin(origin);
+    if (!cb) cb = noop;
+    cb = once(cb);
+    var pending = 3, results = {};
+    self.db.get('request!' + origin, onreq);
+    self.db.get('origin!' + origin, onorigin);
+    self.db.get('block!' + origin, onblock);
+    
+    function onblock (err) {
+        if (err && err.type !== 'NotFoundError') return cb(err);
+        results.block = err ? false : true;
+        if (-- pending === 0) done();
+    }
+    function onorigin (err, perms) {
+        if (err && err.type !== 'NotFoundError') return cb(err);
+        results.perms = perms;
+        if (-- pending === 0) done();
+    }
+    function onreq (err, req) {
+        if (err && err.type !== 'NotFoundError') return cb(err);
+        results.request = req;
+        if (-- pending === 0) done();
+    }
+    function done () {
+        if (results.origin) return cb(null, results.origin)
+        else if (results.block) return cb(null, false)
+        self.db.put('request!' + origin, req, function (err) {
+            if (err) return cb(err);
+            cb(null);
+            self.emit('request', origin, req);
+        });
+    }
+};
+
+Box.prototype.approveRequest = function (origin, cb) {
+    var self = this;
+    origin = normOrigin(origin);
+    if (!cb) cb = noop;
+    self.db.get('request!' + origin, function (err, perms) {
+        if (err) return cb(err);
+        self.db.batch([
+            { type: 'del', key: 'request!' + origin },
+            { type: 'put', key: 'origin!' + origin, value: perms }
+        ], onbatch);
+        function onbatch (err) {
+            if (err) return cb(err);
+            cb(null, perms);
+            self.emit('approve', origin, perms);
+        }
+    });
+};
+
+Box.prototype.rejectRequest = function (origin, cb) {
+    var self = this;
+    if (!cb) cb = noop;
+    self.db.del('request!' + origin, function (err) {
+        if (err) return cb(err);
+        cb(null);
+        self.emit('reject', origin);
+    });
+};
+
+Box.prototype.revokeOrigin = function (origin, cb) {
+    var self = this;
+    if (!cb) cb = noop;
+    self.db.del('origin!' + origin, function (err) {
+        if (err) return cb(err);
+        cb(null);
+        self.emit('revoke', origin);
+    });
+};
+
+Box.prototype.blockOrigin = function (origin, cb) {
+    var self = this;
+    self.db.put('block!' + origin, function (err) {
+        cb(null);
+    });
+};
+
+Box.prototype.listRequests = function (cb) {
+    cb = once(cb);
+    var r = this.db.createReadStream({ gt: 'request!', lt: 'request!~' });
+    var results = cb ? [] : null;
+    
+    var stream = pump(r, through.obj(function (row, enc, next) {
+        var rec = xtend(row.value, {
+            origin: row.key.split('!')[1],
+        });
+        if (results) results.push(rec);
+        this.push(rec);
+        next();
+    }));
+    if (cb) {
+        stream.once('error', cb);
+        stream.on('end', function () { cb(null, results) });
+        process.nextTick(function () { stream.resume() });
+    }
+    return readonly(stream);
+};
 
 Box.prototype.createWallet = function (opts, cb) {
     if (typeof opts === 'function') {
@@ -39,8 +151,9 @@ Box.prototype.createWallet = function (opts, cb) {
     var addr = keypair.getAddress().toString();
     var wif = keypair.toWIF();
     var rec = { address: addr, wif: wif };
+    var value = { wif: wif };
     
-    this.db.put('wallet!' + addr, wif, function (err) {
+    this.db.put('wallet!' + addr, value, function (err) {
         if (err) cb(err)
         else cb(null, rec)
     });
@@ -52,10 +165,9 @@ Box.prototype.listWallets = function (cb) {
     var results = cb ? [] : null;
     
     var stream = pump(r, through.obj(function (row, enc, next) {
-        var rec = {
+        var rec = xtend(row.value, {
             address: row.key.split('!')[1],
-            wif: row.value
-        };
+        });
         if (results) results.push(rec);
         this.push(rec);
         next();
