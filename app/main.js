662f39b74af58a20a40c87ea190587d3b07a6e5a
James Halliday <mail@substack.net>
2015-10-19 14:36:30 -0700

    rpc authentication works

--- app/main.js
+++ app/main.js
@@ -38,18 +38,37 @@ catcher(window, showPage)
 var hello = require('hello-frame-rpc')
 ;(function () {
   var rpc, methods = {}
-  methods.request = function (req, cb) {
-    if (!allowed()) return cb('not authorized')
+  methods.request = allowed(function (req, cb) {
     cb(null, 'whatever')
-  }
+  })
   hello.listen('*', methods, function (err, r) { rpc = r })
-  function allowed () {
-    var origin = normOrigin(rpc.origin)
-console.log(state.access) 
-    for (var i = 0; i < state.access.length; i++) {
-console.log('CMP', normOrigin(state.access[i].origin), origin)
-      if (normOrigin(state.access[i].origin) === origin) return true
+
+  function allowed (f) {
+    return function () {
+      var args = arguments
+      var cb = typeof args[args.length-1] === 'function'
+        ? args[args.length-1] : noop
+
+      if (state.access.length === 0) {
+        box.listAccess(function (err, apps) {
+          if (err) cb(err)
+          else check(apps)
+        })
+      } else check(state.access.length)
+
+      function check (apps) {
+        if (checker(apps)) f.apply(null, args)
+        else cb('not authorized')
+      }
+      function checker (apps) {
+        var origin = normOrigin(rpc.origin)
+        for (var i = 0; i < apps.length; i++) {
+          if (normOrigin(apps[i].origin) === origin) return true
+        }
+        return false
+      }
     }
-    return false
   }
 })()
+
+function noop () {}
